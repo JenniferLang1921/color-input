85ec6bdee58aa6930444ff111980237624dda3cf
Jennifer Lang <jenniferlang1921@gmail.com>
2022-03-02 13:58:05 -0600

    first  commit

---
+++ src/Input.js
@@ -0,0 +1,29 @@
+import colorNames from 'colornames'
+
+const Input = ({ colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText }) => {
+  return (
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>Add Color Name</label>
+        <input 
+            autoFocus
+            id='color'
+            type='text'
+            placeholder='Add color name'
+            requiredvalue={colorValue}
+            onChange={(e) => {
+                setColorValue(e.target.value);
+                setHexValue(colorNames(e.target.value));
+            }}
+        />
+        <button 
+            type="button"
+            onClick={() => setIsDarkText(!isDarkText)}
+            >
+                Toggle Text Color
+            </button>
+    </form>
+    
+  )
+}
+
+export default Input
